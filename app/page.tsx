883d49113036673731d760721c6d43b71e517338
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2025-08-07 10:18:29 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from 'react'
-import { LandingPage } from '@/components/landing-page'
+// import { LandingPage } from '@/components/landing-page'
+import { WaitingPage } from '@/components/waiting-page'
 
 export default function Home() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <WaitingPage/>
     </Suspense>
   )
 }
